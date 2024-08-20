95940af596aa6bc615e9de2abf74254257dc8bbb
tali13 <talitanaibertdacosta@gmail.com>
2024-08-20 16:53:38 -0300

    Add files via upload

---
+++ aula7/aula7.js
@@ -0,0 +1,122 @@
+//aula6
+/*
+let resultado = 100
+resultado = resultado / 10 
+console.log(resultado)
+
+let resultado1 = 100 
+resultado1 /= 10
+console.log(resultado1)
+*/
+
+//aula7
+/*
+ const primeiroValor = true
+ const segundoValor = true
+  const resultado = primeiroValor&&segundoValor
+  
+  console.log(resultado)
+
+
+const a = true 
+const b = false
+const c = true
+
+const resultado = a && b 
+console.log(resultado)
+
+
+const resultado1 = b && c 
+console.log(resultado1)
+
+const resultado2 = a && c 
+console.log(resultado2)
+
+const resultado3 = a && b && c 
+console.log(resultado3)
+*/
+/*
+const valor1 = true 
+const valor2 = true 
+ const resultado4 = valor1 || valor2 
+ console.log(resultado4)
+
+const resultado5 = a || b 
+console.log(resultado5)
+
+
+
+const resultado6 = b || c 
+console.log(resultado6)
+
+
+
+const resultado7 = a || c 
+console.log(resultado7)
+
+
+
+
+
+const resultado8 = a || b || c 
+console.log(resultado8)
+
+*/
+/*
+const a = true 
+const b = false
+const c = true
+
+const valor1 = true 
+ 
+const resultado = !valor1
+
+console.log(resultado)
+*/
+/*
+let nome = prompt("diga seu nome?") 
+console.log("nome", nome)
+
+
+const anosDenascimento = prompt("ano de nascimento?")
+const anoAtual = prompt("ano atual?")
+
+const idade = anoAtual-anosDenascimento
+console.log ("idade",idade)
+
+const primeiroValor = true
+const segundoValor = false 
+
+const maiorDeidade=idade>=18 
+console.log("e maior de idade?", maiorDeidade)
+const anos= 2050 - anoAtual
+console.log("idade em 2050:", idade + anos)
+*/
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+ let resultado = bool1 && bool2
+ console.log("a. ", resultado)
+resultado = bool1 && bool2 && bool3
+console.log("b. ", resultado)
+resultado = !resultado && (bool1 || bool2)
+console.log("C. ", resultado)
+  console.log("d. ",typeof resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
