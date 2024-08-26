9ddcda39b3f80880589ce2db58a45b80a0124a75
tali13 <talitanaibertdacosta@gmail.com>
2024-08-26 17:13:18 -0300

    Add files via upload

---
+++ aulanova.js
@@ -0,0 +1,84 @@
+//aula9
+/*
+const listaNumeros = [1,2,3,4,5,6]
+console.log(listaNumeros.length)
+listaNumeros.push(7)
+listaNumeros.splice(3,2)
+console.log(listaNumeros.length)
+console.log(listaNumeros)
+*/
+/*
+let array
+//sao lista de elementos 
+console.log('a. ',array)
+
+array = null 
+//vazia
+console.log('b. ',array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+//length me diz qual é o tamanho de uma string ou a quantidade de uma array
+let i = 0
+console.log('d. ',array [i])
+
+array[i+1] = 19
+console.log('e.',array)
+
+const valor = array[i+6]
+console.log('f. ',valor)
+*/
+/*
+const frase = prompt("Subi num onibus em marrocos")
+ //para iniciar uma frase  
+console.log(frase.toUpperCase().replaceAll("A", "i"), frase.length)
+console.log recebe a variavel 
+toUpperCase torna todas as letras maiusculas
+replaceAll troca a frese original para uma nova 
+length me diz qual é o tamanho de uma string
+*/
+
+const nomeUsuario = print("seu nome")
+const emailUsuario = prompt("seu email")
+
+const frase = "o email", emailUsuario ,'seja bem-vindo'
+
+
+const frutasFavoritas = ["banana","melancia","amora","limão","mamão"]
+const comidasFavoritas = ["pizza","xis","pastel","coxinha","batata frita"]
+console.log(frutasFavoritas)
+console.log(comidasFavoritas)
+console.log("essas são suas comidas favoritas:")
+console.log(comidasFavoritas[0])
+console.log(comidasFavoritas[1])
+console.log(comidasFavoritas[2])
+console.log(comidasFavoritas[3])
+console.log(comidasFavoritas[4])
+console.log(comidasFavoritas[5])
+comidasFavoritas[1]=prompt("qual sua comida favorita?")
+
+listaDetarefas=[]
+console.log=prompt("quais suas 3 tarefas")
+console.log(listaDetarefas[0])
+console.log(listaDetarefas[1])
+console.log(listaDetarefas[2])
+console.log(listaDetarefas)
+
+console.log=prompt("qual dessas tarefas forem ja feitas?" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
