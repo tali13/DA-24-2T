9fc8b7ab1a76c79c51332b29f8efa98050b39eeb
tali13 <talitanaibertdacosta@gmail.com>
2024-08-27 17:15:49 -0300

    Add files via upload

---
+++ aula10.js
@@ -0,0 +1,171 @@
+/*let condicao1 =true
+if(condicao1){
+    //ação{}
+    console.log("exibiu condição1")
+}
+let condicao2 = false
+if (condicao2){
+    console.log("exibiu condicao2")
+
+}
+*/
+/*
+
+const num1 = Number (prompt("digite o seu primeiro numero;"))
+
+if(num1===num2){
+    console.log("os numeros sao iguais?")
+}
+if(num1===num2){
+    console.log("os numeros sao diferentes")
+}*/
+
+//let condicao = false
+
+//if (condicao){
+  //  console.log("verdadeira")
+//}else{
+//    console.log("falsa") 
+//}
+/*
+const num1 = Number (prompt("digite o seu primeiro numero;"))
+const num2 = Number(prompt("digite o seu segumdo numero"))
+
+if(num1===num2){
+    console.log("os numeros sao iguais")
+}else{
+    console.log("os numeros sao diferentes")
+}
+*/
+/*
+let condicao1 = false
+let condicao2 = false
+if(condicao1){
+    console.log("condicao1 e vdd")
+
+}else if(condicao2){
+        console.log("condicao2 e vdd")
+}else{
+    console.log("entrou no else")
+}
+*/
+/*
+const num1 = Number(prompt("primeiro numero?"))
+const num2 = Number(prompt("segundo numero"))
+if(num1===num2){
+    console.log("os numeros sao iguais")
+}else if(num1>num2){
+    console.log("o 1numero e maior q o segumdo numero")
+}else{
+    console.log("o numero 1 e maior que o numero 2")
+}
+*/
+/*
+let paisDeorigem=prompt("seu pais de origem")
+
+switch(paisDeorigem){
+    case 'Brasil':
+        console.log("brasileiro")
+        break
+        case "Brasil":
+            console.log("brasileiro")
+            break
+            case "Inglaterra":
+                console.log("norte americano")
+                break
+                default :
+                console.log("nenhuma nacionalidade")
+                break
+}
+*/
+/*
+let pokemon=prompt("escolha seu pokemon")
+switch(pokemon){
+    case 'Eevee':
+    console.log("Eevee foi escolhida")
+    break
+    case 'Lucario':
+        console.log("lucario foi escolhido")
+        case 'Piplup':
+            console.log("piplup foi escolhido")
+            break
+            default:
+                console.log("pokemon não identificado")
+                break
+}
+*/
+
+//herry potter
+let herryPotter = prompt("escolha sua casa")
+switch(herryPotter){
+    case 'hermione':
+     console.log("Grifinoria") 
+      break
+        case 'snape':
+          console.log("lufa-lifa")
+            case'draco':
+             console.log("corvinal")
+              case 'herry':
+                console.log("sonserina")
+                 break
+                 default:
+                    console.log("voce não foi escolhido")
+                    break
+}
+
+
+/*
+let condicao=true
+let condicao1=false
+if(condicao1&&condicao1){
+    console.log("as duas sao vdd")
+}else{
+    console.log("todas vdd")
+}
+if(!condicao1){
+    console.log("condicao1 falsa")
+} else{
+    console.log("a condicacao e vdd")
+}
+
+*/
+
+
+
+
+
+const ensinoMedio = prompt("vc ja completou o ensino medio?")
+const idade = prompt("vc e maior de idade?")
+const faculdade= prompt("vc ja esta cursando uma faculdade?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
