bb3b31455908760ffcc1a1914bb287a415c46533
tali13 <talitanaibertdacosta@gmail.com>
2024-09-02 17:14:39 -0300

    Add files via upload

---
+++ aula12.js
@@ -0,0 +1,187 @@
+
+/*
+let i = 0
+while(i<10){
+    console.log("o numero e:",i)
+    i++;
+}
+    */
+   /*
+   let estomago = 0
+   while(estomago<100){
+    console.log("quero comer mais coxinhas", estomago)
+    estomago = estomago + 10
+   }
+    */
+/*
+    let numero ,soma=0
+    while (numero!==0){numero=Number(prompt("coloque o numero:"))
+       soma+=numero
+        console.log("o numero e:", numero)
+
+    }
+
+console.log(soma)
+*/
+/*
+let c = 1
+do{
+
+    console.log("o valor da conta e;", c)
+    c++
+
+}
+*/
+/*
+let numero = [14,67,89,15,23]
+for (let i = 0; i <5;i++) {
+console.log(numero[i])
+    
+}5
+*/ 
+/*
+let numero = [11,15,18,14,21,13]
+let numeroMaior = numero[0]
+for ( let i = 0; i<6;i++){
+    if(numeroMaior<numero[i]){
+        numeroMaior=numero[i]
+    }
+}
+console.log("o numero maior e:",numeroMaior)
+
+
+*/
+/*
+let numero = [11,15,18,14,12,13]
+let soma = 0
+
+for(let i = 0; i<6;i++){
+    if(numero[i]%2===0){
+        soma += numero[i]
+    }
+}
+console.log("a soma dos numeros pares e:"soma)
+*/
+/*
+const numeros = [14,67,89,15,23]
+for (let cafe of numeros){
+    console.log(cafe)
+}
+
+*/
+/*
+let frase=''
+const mensagem = ["oi","sumido","tudo","bem?","saudades"]
+for(let palavra of mensagem){
+frase +=palavra+" "
+}
+console.log(frase)
+*/
+/*
+let numero = [11,15,18,14,12,13]
+let soma = 0
+for(let valor of numero){
+   if(valor%2===0){
+    soma += valor 
+   }
+}
+console.log(soma)
+*/
+let palavra[filme,serie,desenhos,animes]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
