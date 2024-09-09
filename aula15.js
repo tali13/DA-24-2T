dc113104069996901f1ebda3f655a4e008018026
tali13 <talitanaibertdacosta@gmail.com>
2024-09-09 17:11:36 -0300

    Add files via upload

---
+++ aula15.js
@@ -0,0 +1,113 @@
+/*function calcularArea(largura,altura){
+    const Area = largura*altura
+    console.log(Area)
+}
+calcularArea(2,3)
+calcularArea(5,4)
+calcularArea(9,3)
+calcularArea(7,4)
+calcularArea(6,1)
+calcularArea(9,4)
+
+function imprimirHolamundo(){
+console.log("ola mundo")
+}
+imprimirHolamundo()
+imprimirHolamundo()
+imprimirHolamundo()
+function imprimirHolamundo(nome){
+     console.log("bem vindo",nome)
+
+}
+imprimirHolamundo("noha")
+imprimirHolamundo("simon")
+*/
+/*
+
+function bemVindo(){
+    console.log("bem vindo turma 24-2T!")
+}
+bemVindo()
+ function soma(numero,numero2){
+    const resultado = numero*numero2
+    console.log(resultado)
+ }
+
+soma(6,4)
+soma(5,9)
+soma(8,2)
+soma(45,6)
+
+
+function hola(nome){
+    console.log(`hola ${nome}`)
+}
+hola('ruby')
+hola('rose')
+hola('steven')
+
+function calcular(numero3,numero4){
+    const rV = numero3*numero4
+    console.log(rV)
+}
+calcular(66,945)
+calcular(654,31)
+calcular(1685,654*/
+/*
+function livia(){
+    console.log("oi livia,eu não sou de fazer pergunta,aparentemente vc parece bem legal escuto as vezes vc falando de pokemon,vc tem cara que olha anime tambem um dia agente pode conversar,eu acho q era isso so,foi mal pela pergunta curta não converso muito,bye")
+}
+livia()
+
+function Area(largura,altura){
+const resultado= largura*altura
+return resultado
+}
+let calcular = Area(65,155)
+console.log(" a area e:",calcular)
+
+
+
+*/
+/*
+function numeros(azul,verde){
+const resultado = azul+verde
+return resultado
+}
+let soma = numeros(654,4869)
+console.log("a soma dos dois numeros são",soma)
+
+const cores=[0,1,2,3]
+function osNumeros(arrayNumeros){
+const novoArray=[]
+novoArray.push(arrayNumeros[0])
+novoArray.push(arrayNumeros.length-1)
+return novoArray
+}
+console.log(osNumeros(cores))
+*//*
+const calcularArea = function(largura,altura){
+    const soma= largura*altura
+    return soma 
+}
+const resultado = calcularArea(65465,61484694)
+console.log("bem vindo",nome){
+    console.log
+}*/
+const calcularArea = (largura,altura) =>{
+    const resultado=largura * altura
+    return resultado
+}
+const area = calcularArea(33,6465)
+console.log("a area e:",area)
+
+const somaN =function(n1,n2){
+    return n1+n2
+}
+console.log("a soma:",somaN(5454,2165))
+const dobraN = (n)=>{
+    return n *2
+}
+const n = 5
+console.log("o dobro de;",n ,"e",dobraN(n))
+
