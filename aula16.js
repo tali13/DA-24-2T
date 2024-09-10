2ec940e1da8ccbcd8f0ab9945fb986c19d3b7748
tali13 <talitanaibertdacosta@gmail.com>
2024-09-10 17:11:01 -0300

    Add files via upload

---
+++ aula16.js
@@ -0,0 +1,32 @@
+/*function minhaFuncao(variavel){
+    return variavel * 5 //aqui esta multiplacando a funcao
+}
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+//aqui esta chamando a funcao
+//b)n ira acontecer nd
+*/
+/*
+let textoDoUsuario = prompt("insira o texto");
+const outraFuncao = function(texto){
+    return texto.toLowerCase().includes("cenoura")
+}
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+//ira retorna para o texto cenoura
+//ira da a resposta true
+//eu cozinho cenoura
+//faço bolo de cenoura
+//piquei cenoura 
+*/
+/*
+function perguntas(){
+    console.log("eu sou a talita,tenho 17 anos,moro em alvorada e sou estudante")
+}
+    */
+   function soma(n1,n2){
+    const resultado = n1 + n2
+    return resultado
+   }
+   
