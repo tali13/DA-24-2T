4f539c2640b291043ad3724b8f25f15f2eabd48b
tali13 <talitanaibertdacosta@gmail.com>
2024-09-16 17:13:36 -0300

    Add files via upload

---
+++ aula17.js
@@ -0,0 +1,149 @@
+/*const professor = {
+    nome:'noah',
+    idade:26,
+    altura:1.80,
+    email:'noah@gmail.com',
+    tarefas:['dar aula','responder duvidas'],
+    contarPiadas:function(){
+        console.log("e pa ve pi pa come?")
+    }
+}
+console.log(professor.idade)
+console.log(professor.nome)
+console.log(professor.altura)
+console.log(professo[email])
+console.log("a tarefa dp prof e ",professor.tarefas[1])
+
+professor.nome = 'simon'
+professor[email] = 'simon@gmail.com'
+
+console.log(professo[email])
+console.log(professor.nome)
+
+/*
+const filme = {
+    direcao:'rebeca sugar joe',
+    ano:2013,
+    nome:'steven universo',
+    assistido:'sim'
+
+}
+console.log(filme.direcao)
+console.log(filme.ano)
+console.log(filme.nome)
+console.log(filme.assistido)
+*/
+/*
+const pessoa = {
+    nome:'alastor',
+    idade:16,
+    generoMusical:'phonk'
+}
+console.log("o meu nome e",nome,"ele tem",idade,"e gosta de",generoMusical)
+*/
+/*
+const donoDopet={
+    nome:'talita',
+    idade:17,
+    pet:{
+        nome:'nega',
+        raca:'vira-lata',
+        idade:4
+    }
+}
+console.log(donoDopet.pet.nome)
+*/
+/*
+const curso ={
+    nome:'front end noturno',
+    linguagens:['js','css','html']
+}
+console.log(curso.linguagens[0])
+curso.numeroEstudantes = 50
+curso['numeroProfessores'] = 10
+console.log(curso.numeroEstudantes)
+console.log(curso.numeroProfessores)
+/*
+const professores =[
+    {nome:"lucas",idade:32,modulo:1},
+    {nome:"leo",idade:29,modulo:2},
+    {nome:"mika",idade:29,modulo:3}
+]
+console.log(professores[0].nome)
+*/
+/*
+const filme = {
+    direcao:'rebeca sugar joe',
+    ano:2013,
+    nome:'steven universo',
+    assistido:'sim',
+     atores:['ator','ator1','ator2']
+}
+console.log(filme.direcao)
+console.log(filme.ano)
+console.log(filme.nome)
+console.log(filme.assistido)
+
+filme.personagens=['steven','connie','rose']
+
+console.log("o personagem",filme.personagens[0],"e do ator",filme.atores[2])
+console.log("o personagem",filme.personagens[1],"e do ator",filme.atores[0])
+console.log("o personagem",filme.personagens[2],"e do ator",filme.atores[1])
+*/
+/*
+const novoUsuario ={
+    ...usuario,
+    nome:'tali',
+    idade:17,
+    cidade:'alvorada'
+}
+
+console.log(novoUsuario)
+const nomeProfs = ['lucas','leo','mika']
+const novaListaProfs = [...nomeProfs]
+console.log(novaListaProfs)
+*/
+
+/*
+const pessoa = {
+    nome:'alastor',
+    idade:16,
+    amigo:{nome:'naru',
+        itemFavorito:'guitarra',
+          idade:17,
+          comidas:['xis','pastel','coxinha']
+    }
+}
+console.log("o nome da pessoa e",pessoa.amigo.nome,"e suas comidas sao",pessoa.amigo.comidas[1],pessoa.amigo.comidas[2],"e",pessoa.amigo.comidas[0],"seu melhor amigo se chama",pessoa.nome,"e tem",pessoa.idade,"anos")
+*/
+
+
+
+
+const personagens={
+    nome:'izuku midoriya',
+    idade:16,
+    individualidade:'one for all',
+    amigo:{
+nome: 'katsuki bakugo',
+           idade:16,
+                individualidade:'explocao',
+                amigo:{
+                 nome:'ochaco uraraka',
+                 idade:15,
+                 individualidade:'gravidade zero'
+                }
+    }
+}
+const novosPersonagens
+
+
+
+
+
+
+
+
+
+
+
