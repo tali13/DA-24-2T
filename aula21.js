5d599b73b2d88c4598d52e4b5ef455c14d4d7d7e
tali13 <talitanaibertdacosta@gmail.com>
2024-09-30 17:13:39 -0300

    Add files via upload

---
+++ aula21.js
@@ -0,0 +1,79 @@
+/*let matriz =[
+    [2,4,6],
+    [8,10,12],
+    [14,16,18]
+];
+console.log(matriz[2][1]);
+//sera impresso 10,por que esta o numero da linha 2 da coluna 1
+*/
+/*
+let matriz=[
+    [1,2],
+    [3,4],
+    [5,6,7,]
+];
+for (let i = 0;i< matriz.length; i++){
+    for (let j =0; j < matriz[i].length; j++){
+        if (matriz[i][j]%2===0){
+            console.log(matriz[i][j]);
+        }
+    }
+}
+//sera exibido 2,4,6 , porque esta pedindo numeros pares.
+*/
+/*
+let matriz = [
+    [5,10,15],
+    [20,25,30],
+    [35,40,45]
+];
+let soma = 0;
+for (let i = 0; i < matriz.length; i++){
+    for(let j = 0; j<matriz[i].length; j++){
+        soma += matriz[i][j]
+    }
+}
+console.log(soma);
+//o valor e 225,ele somou todos os numeros.
+
+/*
+let matriz=[
+    [0,1,2,0,1],
+    [0,1,2,0,1],
+    [0,1,2,0,1],
+    [0,1,2,0,1],
+    [0,1,2,0,1]
+]
+console.log(matriz[0]);
+*/
+let jurassicPark=[
+    [0,0,1,0,1,0,0,1,2,0]
+    [1,2,2,2,2,1,0,1,1,0],
+    [0,0,0,1,2,2,4,1,0,0],
+    [0,0,0,2,0,2,4,3,0,0],
+    [0,0,0,1,0,2,0,0,2,0],
+    [0,0,1,1,0,2,2,0,0,0],
+    [0,0,1,0,2,2,3,1,0,0],
+    [0,0,1,0,0,2,0,0,0,0],
+    [0,0,0,2,0,2,1,0,2,1],
+    [0,0,0,0,2,2,0,0,1,1]
+    
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
