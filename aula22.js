768184bc232567b8b4e0c022b8191aaa34c7390a
tali13 <talitanaibertdacosta@gmail.com>
2024-10-01 15:44:17 -0300

    Add files via upload

---
+++ aula22.js
@@ -0,0 +1,211 @@
+/*let numero = [1,2,3,4,5];
+let soma=0
+
+for (let i = 0; i < numero.length; i++){
+    soma +=numero[i];
+}
+console.log(soma);
+*/
+/*
+function fatorial(n){
+    let resultado = 1;
+    for (let i = n;i > 0; i--){
+        resultado*=i;
+    }
+    return resultado;
+}
+console.log(fatorial(5));
+*/
+/*
+________________________________________________
+|  iteração   |  i      |  resultado            |
+|    1        |  5      |           120         |
+|             |         |                       |
+|             |         |                       |
+|             |         |                       |
+_________________________________________________
+
+
+*/
+/*
+let numero = [1,2,3,4,5];
+let somaPares = 0
+ for(let i=0; i< numero.length; i++){
+    if (numero[i]%2 === 0){
+        somaPares += numero[i];
+    }
+ }
+
+console.log(somaPares);
+*/
+/*
+function jogoPontuacao(jogadas){
+    let pontuacao = 0;
+
+    for (let i = 0; i < jogadas.length; i++){
+        if(jogadas [i]==="ganhou"){
+            pontuacao+=3;
+        }else if(jogadas[i]==="empatou"){
+            pontuacao += 1;
+        }else if (jogadas[i]==="perdeu"){
+            pontuacao-=1;
+        }
+    }
+    return pontuacao;
+}
+let resultado=jogoPontuacao(["ganhou","empatou","perdeu","ganhou"]);
+console.log(`pontuaçao final:${resultado}`);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
